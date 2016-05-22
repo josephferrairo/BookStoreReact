a198ce1991a001f96d7527d8280c18b223777434
josephferrairo <josephferrairo@gmail.com>
2016-05-22 19:36:28 -0400

    added book select functionality

--- src/containers/book-detail.js
+++ src/containers/book-detail.js
@@ -3,9 +3,17 @@ import { connect } from 'react-redux';
 
 class BookDetail extends Component {
   render() {
+    if (!this.props.book) {
+      return <div>Select a book to get started.</div>
+    }
+
     return (
       <div>
-        <div>Book Detail</div>
+        <div>
+          <h3>Details For:</h3>
+          <div>{this.props.book.title}</div>
+          <div>Pages: {this.props.book.pages}</div>
+        </div>
       </div>
     );
   }
